fa6f1a306241166da0479c296000f2f5e33aefb3
Hatice Kilicoglu <yoiyoei@135.166.109.145.hva.nl>
2019-11-15 15:47:44 +0100

    re

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
-/*eslint-env browser*/
-/*eslint 'no-console': 0*/
-// store elements in variables
 var zoekveld = document.querySelector('header form');
 var zoekbutton = document.querySelector('.zoeken');
 
